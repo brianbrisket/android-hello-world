9636854db5cfbb5f63e04b681cbad9acd05611ca
someone <someone@Someones-MacBook-Air.local>
2018-01-05 21:45:30 -0800

    Add test function to check Hello World greeting

--- app/src/androidTest/java/com/sqisland/android/hello/MainActivityTest.java
+++ app/src/androidTest/java/com/sqisland/android/hello/MainActivityTest.java
@@ -4,9 +4,13 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by someone on 1/5/18.
@@ -17,4 +21,9 @@ public class MainActivityTest {
     public ActivityTestRule<MainActivity> activityTestRule
             = new ActivityTestRule<>(MainActivity.class);
 
+    @Test
+    public void bye() {
+        onView(withId(R.id.greeting)
+                .check(matches(withText(R.string.hello_world)));
+    }
 }
